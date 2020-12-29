be811aff67019735a504e652625a9f74d581147f
Ketul <ptl.ketul@gmail.com>
2020-12-29 19:27:36 +0000

    producer unit test

--- src/test/java/com/ketul/kafka/producer/BankTransactionProducerTest.java
+++ src/test/java/com/ketul/kafka/producer/BankTransactionProducerTest.java
@@ -5,10 +5,7 @@ import com.ketul.kafka.message.BankTransaction;
 import com.ketul.kafka.serde.BankTransactionSerializer;
 import com.ketul.kafka.stream.BankBalanceStreamApplication;
 import com.ketul.kafka.utils.StreamConstants;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.MockProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.Topology;
 import org.junit.After;
@@ -18,18 +15,43 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.Assert.*;
 
 public class BankTransactionProducerTest {
 
+    private BankTransactionProducer bankTransactionProducer = new BankTransactionProducer();
     private MockProducer<String, BankTransaction> mockProducer;
     private ObjectMapper mapper = new ObjectMapper();
+    private String[] customers = new String[]{"John", "Thomas", "Ketul", "Jacob", "Bhumika", "Vipul"};
+
     @Before
     public void setUp() throws Exception {
         mockProducer = new MockProducer<>(true, new StringSerializer(), new BankTransactionSerializer(mapper));
+    }
+
+    @Test
+    public void testProducer() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = BankTransactionProducer.class.getDeclaredMethod("produceTransactions", Producer.class, boolean.class);
+        method.setAccessible(true);
+        method.invoke(bankTransactionProducer, mockProducer, false);
+
+        List<ProducerRecord<String, BankTransaction>> producedTransactions = mockProducer.history();
+
+        Assert.assertEquals(producedTransactions.size(), 10000);
+
+        for(ProducerRecord<String, BankTransaction> producedTransaction : producedTransactions) {
+            Assert.assertTrue(Arrays.asList(customers).contains(producedTransaction.key()));
+            Assert.assertTrue(producedTransaction.value().getName().equals(producedTransaction.key()));
+            Assert.assertTrue(producedTransaction.value().getAmount() < 1000f && producedTransaction.value().getAmount() > 0);
+            Assert.assertTrue(!(producedTransaction.value().getTime().compareTo(Instant.now()) == 1));
+        }
+
     }
 
     @Test
