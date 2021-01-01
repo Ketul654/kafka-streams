27d2aab28b1a191f8f51dc734bae46f862c86d11
Ketul <ptl.ketul@gmail.com>
2021-01-01 16:21:07 +0000

    ma and map values

---
+++ src/main/java/com/ketul/kafka/stream/transform/stateless/StreamMap.java
@@ -0,0 +1,70 @@
+package com.ketul.kafka.stream.transform.stateless;
+
+import com.ketul.kafka.message.Customer;
+import com.ketul.kafka.serde.CustomerDeserializer;
+import com.ketul.kafka.serde.CustomerSerializer;
+import com.ketul.kafka.utils.StreamConstants;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Properties;
+
+/**
+ * Topic -> Stream -> Map -> MapValues -> Topic
+ * <p>
+ * 1. Create input and output topic
+ * bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic customer-input --replication-factor 3 --partitions 3
+ * bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic customer-output --replication-factor 3 --partitions 3
+ * <p>
+ * 2. Start Customer Producer
+ * <p>
+ * 3. Start this stream. You can also start multiple instances of stream
+ */
+public class StreamMap {
+    private static final Logger logger = LoggerFactory.getLogger(StreamMap.class);
+
+    public static void main(String[] args) {
+        Properties properties = getStreamProperties();
+        Topology topology = createTopology();
+        logger.info(topology.describe().toString());
+        KafkaStreams streams = new KafkaStreams(topology, properties);
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private static Topology createTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
+        Serde<Customer> customerSerdes = Serdes.serdeFrom(new CustomerSerializer(), new CustomerDeserializer());
+        KStream<String, Customer> customerKStream = builder.stream(StreamConstants.INPUT_TOPIC,
+                // Explicitly declaring serdes.
+                Consumed.with(
+                        Serdes.String(),
+                        customerSerdes // Custom sedes
+                ))
+                .filter(((customerId, customer) -> customer.getAge() >= 25))
+                .filterNot(((customerId, customer) -> customerId.startsWith("u")))
+                /*
+                This will mark stream data for re-partitioning. Applying group by or join after this triggers re-partitioning.
+                That is the reason mapValues is preferred over map.
+                 */
+                .map(((customerId, customer) -> KeyValue.pair(customerId.toUpperCase(), customer)))
+                .mapValues((Customer customer) -> new Customer(customer.getCustomerId(), customer.getName(), customer.getAge(), null));
+
+        customerKStream.foreach((customerId, customer) -> logger.info(String.format("%s=>%s", customerId, customer.toString())));
+        return builder.build();
+    }
+
+    private static Properties getStreamProperties() {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, StreamConstants.APPLICATION_ID);
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, StreamConstants.BOOTSTRAP_SERVERS);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, StreamConstants.AUTO_OFFSET_RESET_EARLIEST);
+        properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
+        return properties;
+    }
+}
